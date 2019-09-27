afb26854bd0767f328f1d04d03013ba6ede63773
C K <cate@caitlen.dev>
2019-09-27 18:06:50 +1000

    Added monster attributes and basic attack method

--- src/Monster.java
+++ src/Monster.java
@@ -1,5 +1,21 @@
 public class Monster {
-	private String monsterName;
+	// Monster properties
+	private String name;
 	private int health;
 	private int damage;
+	
+	// Constructor
+	public Monster(String name, int health, int damage) {
+		this.name = "Monster";
+		this.health = 30;
+		this.damage = 2;
+	}
+	
+	// Attack
+	public String Attack(Monster opponent) {
+		while(true) {
+			opponent.health -= this.damage;
+			return String.format("%s attacks %s for 2 damage!", this.name, opponent.name, this.damage);
+		}
+	}
 }
